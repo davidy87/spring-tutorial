5797e35ad6d38e0219ca9dff08148f03d27ca2d6
David Yoon <dyoon0807@gmail.com>
2023-06-26 21:07:38 +0900

    DB changed to MySQL & minor code fix

--- user-management/src/main/java/example/usermanagement/SpringConfig.java
+++ user-management/src/main/java/example/usermanagement/SpringConfig.java
@@ -1,14 +1,27 @@
 package example.usermanagement;
 
-import example.usermanagement.aop.TimeTraceAop;
+import example.usermanagement.repository.JdbcTemplateMemberRepository;
+import example.usermanagement.repository.JpaMemberRepository;
 import example.usermanagement.repository.MemberRepository;
 import example.usermanagement.service.MemberService;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+//    private final DataSource dataSource;
+//    private final EntityManager em;
+//
+//    public SpringConfig(DataSource dataSource, EntityManager em) {
+//        this.dataSource = dataSource;
+//        this.em = em;
+//    }
+
 
     private final MemberRepository memberRepository;
 
@@ -18,14 +31,16 @@ public class SpringConfig {
     }
 
     @Bean
-    public MemberService nemberService() {
+    public MemberService memberService() {
         return new MemberService(memberRepository);
     }
 
 //    @Bean
 //    public MemberRepository memberRepository() {
 ////        return new MemoryMemberRepository();
+////        return new JdbcTemplateMemberRepository(dataSource);
 ////        return new JpaMemberRepository(em);
+//
 //    }
 
     // For AOP
--- user-management/src/main/java/example/usermanagement/aop/TimeTraceAop.java
+++ user-management/src/main/java/example/usermanagement/aop/TimeTraceAop.java
@@ -1,26 +1,26 @@
-package example.usermanagement.aop;
-
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
-
-@Aspect
-@Component // Do either this or add a bean to config.
-public class TimeTraceAop {
-
-    @Around("execution(* example.usermanagement..*(..))")
-    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
-        long start = System.currentTimeMillis();
-        System.out.println("START: " + joinPoint.toString());
-
-        try {
-            return joinPoint.proceed();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
-        }
-
-    }
-}
+//package example.usermanagement.aop;
+//
+//import org.aspectj.lang.ProceedingJoinPoint;
+//import org.aspectj.lang.annotation.Around;
+//import org.aspectj.lang.annotation.Aspect;
+//import org.springframework.stereotype.Component;
+//
+//@Aspect
+//@Component // Do either this or add a bean to config.
+//public class TimeTraceAop {
+//
+//    @Around("execution(* example.usermanagement..*(..))")
+//    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+//        long start = System.currentTimeMillis();
+//        System.out.println("START: " + joinPoint.toString());
+//
+//        try {
+//            return joinPoint.proceed();
+//        } finally {
+//            long finish = System.currentTimeMillis();
+//            long timeMs = finish - start;
+//            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+//        }
+//
+//    }
+//}
