f666ecd47b234aa6de42491cd2807e6a263c6236
David Yoon <dyoon0807@gmail.com>
2023-06-27 16:55:41 +0900

    AOP code updated

--- user-management/src/main/java/example/usermanagement/aop/TimeTraceAop.java
+++ user-management/src/main/java/example/usermanagement/aop/TimeTraceAop.java
@@ -1,26 +1,31 @@
-//package example.usermanagement.aop;
-//
-//import org.aspectj.lang.ProceedingJoinPoint;
-//import org.aspectj.lang.annotation.Around;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.springframework.stereotype.Component;
-//
-//@Aspect
-//@Component // Do either this or add a bean to config.
-//public class TimeTraceAop {
-//
-//    @Around("execution(* example.usermanagement..*(..))")
-//    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
-//        long start = System.currentTimeMillis();
-//        System.out.println("START: " + joinPoint.toString());
-//
-//        try {
-//            return joinPoint.proceed();
-//        } finally {
-//            long finish = System.currentTimeMillis();
-//            long timeMs = finish - start;
-//            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
-//        }
-//
-//    }
-//}
+package example.usermanagement.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+
+@Aspect
+@Component // Do either this or add a bean to config.
+public class TimeTraceAop {
+
+    Logger logger = LoggerFactory.getLogger(TimeTraceAop.class);
+
+    @Around("execution(* example.usermanagement..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        StopWatch stopwatch = new StopWatch();
+        stopwatch.start();
+
+        try {
+            return joinPoint.proceed();
+        } finally {
+            stopwatch.stop();
+            logger.info(stopwatch.prettyPrint());
+        }
+    }
+
+}
